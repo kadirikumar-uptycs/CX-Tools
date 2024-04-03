3a9c7678a817ce65996e28d89b508a072839df08
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-04-03 20:34:21 +0530

    Updated Server URL to HTTPS

--- frontend-app/src/config.js
+++ frontend-app/src/config.js
@@ -1,6 +1,7 @@
 let config = {};
 
-config.SERVER_BASE_ADDRESS = "http://3.92.96.200:17291";
+// config.SERVER_BASE_ADDRESS = "http://3.92.96.200:17291";
+config.SERVER_BASE_ADDRESS = "https://server-cxtools.uptycs.dev";
 // config.SERVER_BASE_ADDRESS = "http://localhost:17291";
 
 export default config;
