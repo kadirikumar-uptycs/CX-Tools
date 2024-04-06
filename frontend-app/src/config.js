598c911e22f11fea65d87f645460c080a2308668
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-04-06 17:23:13 +0530

    Updated config for Development and Production

--- frontend-app/src/config.js
+++ frontend-app/src/config.js
@@ -1,7 +1,9 @@
 let config = {};
 
 // config.SERVER_BASE_ADDRESS = "http://3.92.96.200:17291";
-config.SERVER_BASE_ADDRESS = "https://server-cxtools.uptycs.dev";
-// config.SERVER_BASE_ADDRESS = "http://localhost:17291";
 
+if (process.env.NODE_ENV === 'production')
+    config.SERVER_BASE_ADDRESS = "https://server-cxtools.uptycs.dev";
+else
+    config.SERVER_BASE_ADDRESS = "http://localhost:17291";
 export default config;
