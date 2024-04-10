5322167545a0bdb1e7ef5937ae4c49c486b9419d
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-04-10 20:29:41 +0530

    Transfered Resources List to seperate File

--- frontend-app/src/components/SelectComponent.jsx
+++ frontend-app/src/components/SelectComponent.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ContextProvider } from './MigrateResources';
 import './css/SelectComponent.css';
+import resourcesList from '../resourcesList';
 
 
 export default function SelectComponent() {
@@ -9,25 +10,14 @@ export default function SelectComponent() {
     let { setState } = useContext(ContextProvider);
     let [searchParams, setSearchParams] = useSearchParams();
     let resourceValue = searchParams.get('resource');
-    let resources = [
-        { name: "Flag Profiles", endpoint: "flagProfiles" },
-        { name: "Exceptions", endpoint: "exceptions" },
-        { name: "Alert Rules", endpoint: "alertRules" },
-        { name: "Event Rules", endpoint: "eventRules" },
-        { name: "Event Exclude Profiles", endpoint: "eventExcludeProfiles" },
-        { name: "FIM Rules", endpoint: "filePathGroups" },
-        { name: 'Dashboards', endpoint: 'customdashboards'},
-        { name: 'Yara Profiles', endpoint: 'yaraGroupRules'},
-        { name: 'Roles', endpoint: 'roles'},
-        { name: 'Custom Profiles', endpoint: 'customProfiles'},
-    ];
+    let resources = resourcesList;
 
     if (!(resources.some(obj => obj.endpoint))) {
         resourceValue = 'flagProfiles';
     }
 
     const handleChange = (event) => {
-        setSearchParams({ resource: event.target.value });
+        setSearchParams({ resource: event?.target?.value });
         setState(prev => ({
             ...prev,
             sourceResources: [],
