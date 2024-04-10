c99edd53858f02188c7f51fb9dc649cecc2df9c7
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-04-10 20:26:58 +0530

    Created seperate file for resources in dropdown list

---
+++ frontend-app/src/resourcesList.js
@@ -0,0 +1,13 @@
+let resources = [
+    { name: "Flag Profiles", endpoint: "flagProfiles" },
+    { name: "Exceptions", endpoint: "exceptions" },
+    { name: "Alert Rules", endpoint: "alertRules" },
+    { name: "Event Rules", endpoint: "eventRules" },
+    { name: "Event Exclude Profiles", endpoint: "eventExcludeProfiles" },
+    { name: "FIM Rules", endpoint: "filePathGroups" },
+    { name: 'Dashboards', endpoint: 'customdashboards'},
+    { name: 'Yara Profiles', endpoint: 'yaraGroupRules'},
+    { name: 'Roles', endpoint: 'roles'},
+    { name: 'Custom Profiles', endpoint: 'customProfiles'},
+];
+export default resources;
