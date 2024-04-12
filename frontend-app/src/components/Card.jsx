2fea04362f2a7f5e617bfee2aee78dc3e20c09b1
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-04-12 20:17:05 +0530

    Changed Animation for Card

--- frontend-app/src/components/Card.jsx
+++ frontend-app/src/components/Card.jsx
@@ -1,24 +1,32 @@
-import React, {useState} from "react";
+import React, { useCallback, useRef } from "react";
 import ScrollTrigger from 'react-scroll-trigger';
 
 
 export default function Card({ index, name, description, imageURL, URL }) {
-    const [isVisible, setIsVisible] = useState(false);
+    let cardRef = useRef(null);
+
+    const sleep = time => new Promise(res => setTimeout(res, time));
+
+    let setanimation = useCallback(async (index) => {
+        await sleep(index * 1500 + 1000);
+        cardRef.current.classList.add('visible');
+    }, [cardRef?.current?.classList]);
 
     return (
-        <ScrollTrigger onEnter={() => setIsVisible(true)}>
-                <div className={`card card-${index} ${isVisible ? 'visible' : ''}`}>
-                    <div className="card-left">
-                        <img src={imageURL} alt="Migrating Flag Profiles" className="card--image" />
-                    </div>
-                    <div className="card-right">
-                        <span className="card--name">{name}</span>
-                        <p className="lead card--description">{description}</p>
-                        <a href={URL}>
-                            <button className="card--button">Try this</button>
-                        </a>
-                    </div>
-                </div>
-        </ScrollTrigger>
+
+        <div className={`card card-${index}`} ref={cardRef}>
+            <div className="card-left">
+                <img src={imageURL} alt="Migrating Flag Profiles" className="card--image" />
+            </div>
+            <div className="card-right">
+                <span className="card--name">{name}</span>
+                <p className="lead card--description">{description}</p>
+                <a href={URL}>
+                    <ScrollTrigger onEnter={setanimation}>
+                        <button className="card--button">Try this</button>
+                    </ScrollTrigger>
+                </a>
+            </div>
+        </div>
     )
 }
