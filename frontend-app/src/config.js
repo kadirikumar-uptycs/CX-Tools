e03cbb911d572f9ad905e638c3f8e1e53a19c122
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-09-13 16:35:55 +0530

    dynamic Server URL

--- frontend-app/src/config.js
+++ frontend-app/src/config.js
@@ -1,6 +1,7 @@
 let config = {};
 
-config.SERVER_BASE_ADDRESS = "http://localhost:17291";
-// config.SERVER_BASE_ADDRESS = "/api";
-
+if (process.env.NODE_ENV === 'production')
+    config.SERVER_BASE_ADDRESS = "https://server-cxtools.uptycs.dev";
+else
+    config.SERVER_BASE_ADDRESS = "http://localhost:17291";
 export default config;
