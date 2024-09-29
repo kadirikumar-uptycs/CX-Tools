595e51ffa7ce1c0989aae22a898796689b537a22
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-09-29 23:17:50 +0530

    Added Osquery Event Analysis

--- frontend-app/src/OsqueryLogAnalysis/FullAnalysis.jsx
+++ frontend-app/src/OsqueryLogAnalysis/FullAnalysis.jsx
@@ -1,62 +1,59 @@
 import * as React from 'react';
 import AccordionGroup from '@mui/joy/AccordionGroup';
-import Accordion from '@mui/joy/Accordion';
-import AccordionDetails, {
+import {
     accordionDetailsClasses,
 } from '@mui/joy/AccordionDetails';
-import AccordionSummary, {
+import {
     accordionSummaryClasses,
 } from '@mui/joy/AccordionSummary';
-import Typography from '@mui/joy/Typography';
-import Avatar from '@mui/joy/Avatar';
-import ListItemContent from '@mui/joy/ListItemContent';
 import InfoIcon from '@mui/icons-material/Info';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import HiveRoundedIcon from '@mui/icons-material/HiveRounded';
+import CustomAccordion from './CustomAccordion';
 import VersionDetails from './VersionDetails';
+import Box from '@mui/joy/Box';
+import IndividualEventsCharts from './IndividualEventsCharts';
+import CombinedEventsCharts from './CombinedEventsCharts';
 
 export default function FullAnalysis({ obj }) {
     return (
-        <AccordionGroup
-            variant="plain"
-            transition="0.2s"
-            sx={{
-                width: 'calc(100% - 50px)',
-                borderRadius: 'md',
-                [`& .${accordionDetailsClasses.content}.${accordionDetailsClasses.expanded}`]:
-                {
-                    paddingBlock: '1rem',
-                },
-                [`& .${accordionSummaryClasses.button}`]: {
-                    paddingBlock: '1rem',
-                },
-            }}
-        >
-            <Accordion>
-                <AccordionSummary
-                    sx={{
-                        background: 'var(--bg-color)',
-                        borderRadius: '5px',
-                    }}
-                >
-                    <Avatar color="primary">
-                        <InfoIcon />
-                    </Avatar>
-                    <ListItemContent>
-                        <Typography level="title-md">Version Details</Typography>
-                        <Typography level="body-sm">
-                        Details of the Operating System, Uptycs Osquery and Platform
-                        </Typography>
-                    </ListItemContent>
-                </AccordionSummary>
-                <AccordionDetails
-                    sx={{
-                        maxHeight: '500px',
-                        backgroundColor: 'rgba(0,0,0, 0.03)',
-                        backdropFilter: 'blur(30px)',
-                    }}
-                >
-                    <VersionDetails versionDetails={obj?.versionDetails} />
-                </AccordionDetails>
-            </Accordion>
-        </AccordionGroup>
+        <Box>
+            <AccordionGroup
+                variant="plain"
+                transition="0.2s"
+                sx={{
+                    width: 'calc(100% - 50px)',
+                    borderRadius: 'md',
+                    [`& .${accordionDetailsClasses.content}.${accordionDetailsClasses.expanded}`]:
+                    {
+                        paddingBlock: '1rem',
+                    },
+                    [`& .${accordionSummaryClasses.button}`]: {
+                        paddingBlock: '1rem',
+                    },
+                    marginBottom: '300px',
+                    gap: '21px'
+                }}
+            >
+                <CustomAccordion
+                    icon={<InfoIcon />}
+                    title={"Version Details"}
+                    description={"Details of the Operating System, Uptycs Osquery and Platform"}
+                    details={<VersionDetails versionDetails={obj?.versionDetails} />}
+                />
+                <CustomAccordion
+                    icon={<EventNoteIcon />}
+                    title={"Individual Events Details"}
+                    description={"Individual details of the different events like process_file_events, registry_events, socket_events etc.."}
+                    details={<IndividualEventsCharts eventDetails={obj?.eventDetails} />}
+                />
+                <CustomAccordion
+                    icon={<HiveRoundedIcon />}
+                    title={"Combined Events Details"}
+                    description={"Combined Events Analysis"}
+                    details={<CombinedEventsCharts eventDetails={obj?.eventDetails} />}
+                />
+            </AccordionGroup>
+        </Box>
     );
 }
