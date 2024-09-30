638348433e97ae2a66078d3b0679a783331886f4
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-09-30 19:33:57 +0530

    Added Link to Home Page

--- frontend-app/src/common/PageNotFound.jsx
+++ frontend-app/src/common/PageNotFound.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import Lottie from 'lottie-react';
+import { Link } from 'react-router-dom';
 import LottieFile from '../assets/images/Lottie/page_not_found.json';
 import { useSnackbar } from '../hooks/SnackBarProvider';
+import ButtonSolid from '../utils/ButtonSolid';
+import Typography from '@mui/joy/Typography';
+import Stack from '@mui/joy/Stack';
+
 const PageNotFound = () => {
     const openSnackbar = useSnackbar();
     useEffect(() => {
@@ -11,11 +16,11 @@ const PageNotFound = () => {
     return (
         <div style={{
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             width: '100%',
             height: '100%',
-            marginLeft: '30px'
         }}>
             <Lottie
                 animationData={LottieFile}
@@ -23,9 +28,15 @@ const PageNotFound = () => {
                 autoplay={true}
                 style={{
                     width: '85%',
-                    height: '100%'
+                    height: '50%',
                 }}
             />
+            <Stack gap={5} alignItems={"center"}>
+                <Typography level="h3" color='danger'>Page Not Found</Typography>
+                <Link to={"/"}>
+                    <ButtonSolid text={"Home Page ➺"} />
+                </Link>
+            </Stack>
         </div>
     );
 }
